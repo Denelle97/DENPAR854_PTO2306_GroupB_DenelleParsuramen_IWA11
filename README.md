22de95f35cd79b716330eee0634c7a1e6e2cbd7d
Denelle97 <denelle728@gmail.com>
2023-11-05 01:19:12 +0200

    completed challenge

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # DENPAR854_PTO2306_GroupB_DenelleParsuramen_IWA11
+
+Why did the original code, provided for this project/challenge, not work?
+
+1.Syntax Errors
+-example; "1-root" - Variables cannot start with a number!
+- (-) invalid character use in variables
+
+2.Incorrect Selectors
+-example; "document(order1)" - is not a valid syntax for selecting an element with the ID "order1".
+The correct syntax is document.querySelector('#order1') or as shown in my code.
+
+3.Missing Quotations
+-Strings were not enclosed in quotes
+
+4.Undefined Variables
+-Variables used without being declared or assigned values.
+
+5.Incorrect Comparison in Ternary Operator
+-The ternary operator used for updating the status did not compare the `\ status`\ correctly.
+
+6. Misuse of "document" and "querySelector"
+-There was confusion in how document and querySelector were used. document is not a method
+
+7.Incorrect Assignment
+-Assignment should use "=" without spaces around it
+
+What changes did you need to make to achieve the intended behaviour?
+
+1.Corrected Selectors
+2.Corrected Variable Naming
+3.Accessing Data Attributes
+4.Updating Text Content
+5.Ternary Operator for Status
+
+Explained more in my code comments
--- scripts.js
+++ scripts.js
@@ -1,34 +1,85 @@
 // script.js
 
-const 1-root = document(order1),
-const 1-biscuits: document(biscuits),
-const 1-donuts: document(donuts),
-const 1-pancakes: document(pancakes),
-const 1-status: document(status)
+// added function to update order details...
+function updateOrderDetails(orderKey) {
+    const root = document.querySelector(`[data-key="${orderKey}"]`); 
+    
+    /*Selected the root element for the specified order key
+    *
+    * Used the "document.querySelector" method with a data attribute selector 
+    * to select the root element for the specified order key.
+    */
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-const 2-root = document(order2),
-const 2-biscuits: document(biscuits),
-const 2-donuts: document(donuts),
-const 2-pancakes: document(pancakes),
-const 2-status: document(status)
+    /* Select specific elements within the order for biscuits, donuts, pancakes, and status
+    *
+    * Used querySelector to select specific elements within the order based on their classes.
+    */
+    const biscuits = root.querySelector('.biscuits .count');
+    const donuts = root.querySelector('.donuts .count');
+    const pancakes = root.querySelector('.pancakes .count');
+    const status = root.querySelector('.status dd');
 
-const 3-root = document(order3),
-const 3-biscuits: document(biscuits),
-const 3-donuts: document(donuts),
-const 3-pancakes: document(pancakes),
-const 3-status: document(status)
+    // Accessing data attributes for the specified order using `getAttribute`
+    const biscuitsCount = root.getAttribute('data-biscuits');
+    const donutsCount = root.getAttribute('data-donuts');
+    const pancakesCount = root.getAttribute('data-pancakes');
+    const deliveredStatus = root.getAttribute('data-delivered');
 
-1-biscuits= 1-root.biscuits,
-1-donuts = 1-root.donuts,
-1-pancakes = 1-root.pancakes,
-1-status = 1-root.status ? Delivered : Pending
+    /**  Updated text content of specific elements with the data attribute values
+     *   Used Template literals
+     * */
+    biscuits.textContent = `Biscuits: ${biscuitsCount}`;
+    donuts.textContent = `Donuts: ${donutsCount}`;
+    pancakes.textContent = `Pancakes: ${pancakesCount}`;
+    status.textContent = deliveredStatus === 'true' ? 'Delivered' : 'Pending';
+}
 
-2-biscuits= 2-root.biscuits,
-2-donuts = 2-root.donuts,
-2-pancakes = 2-root.pancakes,
-2-status = 2-root.status ? Delivered : Pending
+// Called the updateOrderDetails function for each order to update their details.
+updateOrderDetails('order1');
+updateOrderDetails('order2');
+updateOrderDetails('order3');
 
-3-biscuits= 3-root.biscuits,
-3-donuts = 3-root.donuts,
-3-pancakes = 3-root.pancakes,
-3-status = 3-root.status ? Delivered : Pending
+///////////////////////////////////////////////////////
+
+//Different/long way of solving this challenge
+
+// Order 1
+// const root1 = document.querySelector('[data-key="order1"]'); // Selected the root element of Order 1
+// const biscuits1 = root1.querySelector('.biscuits .count'); // Selected  the count element for biscuits in Order 1
+// const donuts1 = root1.querySelector('.donuts .count'); // Selected  the count element for donuts in Order 1
+// const pancakes1 = root1.querySelector('.pancakes .count'); // Selected  the count element for pancakes in Order 1
+// const status1 = root1.querySelector('.status .count'); // Selected  the count element for status in Order 1
+
+// // Order 2
+// const root2 = document.querySelector('[data-key="order2"]'); // Selected  the root element of Order 2
+// const biscuits2 = root2.querySelector('.biscuits .count'); // Selected  the count element for biscuits in Order 2
+// const donuts2 = root2.querySelector('.donuts .count'); // Selected  the count element for donuts in Order 2
+// const pancakes2 = root2.querySelector('.pancakes .count'); // Selected  the count element for pancakes in Order 2
+// const status2 = root2.querySelector('.status .count'); // Selected  the count element for status in Order 2
+
+// // Order 3
+// const root3 = document.querySelector('[data-key="order3"]'); // Selected  the root element of Order 3
+// const biscuits3 = root3.querySelector('.biscuits .count'); // Selected the count element for biscuits in Order 3
+// const donuts3 = root3.querySelector('.donuts .count'); // Selected the count element for donuts in Order 3
+// const pancakes3 = root3.querySelector('.pancakes .count'); // Selected the count element for pancakes in Order 3
+// const status3 = root3.querySelector('.status .count'); // Selected the count element for status in Order 3
+
+// // Update values based on data attributes
+// // Order 1
+// biscuits1.textContent = root1.getAttribute('data-biscuits'); // Set the count for biscuits in Order 1
+// donuts1.textContent = root1.getAttribute('data-donuts'); // Set the count for donuts in Order 1
+// pancakes1.textContent = root1.getAttribute('data-pancakes'); // Set the count for pancakes in Order 1
+// status1.textContent = root1.getAttribute('data-delivered') === 'true' ? 'Delivered' : 'Pending'; // Set the status in Order 1
+
+// // Order 2
+// biscuits2.textContent = root2.getAttribute('data-biscuits'); // Set the count for biscuits in Order 2
+// donuts2.textContent = root2.getAttribute('data-donuts'); // Set the count for donuts in Order 2
+// pancakes2.textContent = root2.getAttribute('data-pancakes'); // Set the count for pancakes in Order 2
+// status2.textContent = root2.getAttribute('data-delivered') === 'true' ? 'Delivered' : 'Pending'; // Set the status in Order 2
+
+// // Order 3
+// biscuits3.textContent = root3.getAttribute('data-biscuits'); // Set the count for biscuits in Order 3
+// donuts3.textContent = root3.getAttribute('data-donuts'); // Set the count for donuts in Order 3
+// pancakes3.textContent = root3.getAttribute('data-pancakes'); // Set the count for pancakes in Order 3
+// status3.textContent = root3.getAttribute('data-delivered') === 'true' ? 'Delivered' : 'Pending'; // Set the status in Order 3
